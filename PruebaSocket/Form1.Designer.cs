d54672d1ef4ef35a2bcd9d96b3b465efb1d8d93d
Josue <josrangel@hotmail.com>
2020-10-10 18:04:18 -0500

    primer commit

---
+++ PruebaSocket/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PruebaSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPuerto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxRespuesta = new System.Windows.Forms.TextBox();
+            this.btnEscuchar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPuerto
+            // 
+            this.textBoxPuerto.Location = new System.Drawing.Point(289, 15);
+            this.textBoxPuerto.Name = "textBoxPuerto";
+            this.textBoxPuerto.Size = new System.Drawing.Size(70, 20);
+            this.textBoxPuerto.TabIndex = 0;
+            this.textBoxPuerto.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puerto: ";
+            // 
+            // textBoxRespuesta
+            // 
+            this.textBoxRespuesta.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxRespuesta.Location = new System.Drawing.Point(16, 47);
+            this.textBoxRespuesta.Multiline = true;
+            this.textBoxRespuesta.Name = "textBoxRespuesta";
+            this.textBoxRespuesta.ReadOnly = true;
+            this.textBoxRespuesta.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxRespuesta.Size = new System.Drawing.Size(424, 230);
+            this.textBoxRespuesta.TabIndex = 2;
+            // 
+            // btnEscuchar
+            // 
+            this.btnEscuchar.Location = new System.Drawing.Point(365, 12);
+            this.btnEscuchar.Name = "btnEscuchar";
+            this.btnEscuchar.Size = new System.Drawing.Size(75, 23);
+            this.btnEscuchar.TabIndex = 3;
+            this.btnEscuchar.Text = "Escuchar";
+            this.btnEscuchar.UseVisualStyleBackColor = true;
+            this.btnEscuchar.Click += new System.EventHandler(this.btnEscuchar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "IP";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(35, 16);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(198, 20);
+            this.textBoxIP.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 289);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEscuchar);
+            this.Controls.Add(this.textBoxRespuesta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPuerto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPuerto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxRespuesta;
+        private System.Windows.Forms.Button btnEscuchar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIP;
+    }
+}
+
---
+++ PruebaSocket/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;               
+using System.Net.Sockets;  
+
+namespace PruebaSocket
+{
+    public partial class Form1 : Form
+    {
+        ThreadStart delegado;
+        Socket miPrimerSocket;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false; 
+            textBoxIP.Text = IPAddress.Any.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEscuchar_Click(object sender, EventArgs e)
+        {
+            delegado = new ThreadStart(Conectar);
+            Thread hilo = new Thread(delegado);
+            //Iniciamos el hilo 
+            hilo.Start(); 
+        }
+
+        public void Conectar()
+        {
+            String iptxt= textBoxIP.Text;
+            int puertotxt= int.Parse(textBoxPuerto.Text);
+            byte[] ByRec;
+            miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint miDireccion = new IPEndPoint(IPAddress.Parse(iptxt), puertotxt);
+
+            try
+            {
+                miPrimerSocket.Bind(miDireccion);
+                miPrimerSocket.Listen(1);
+
+                textBoxRespuesta.Text += "Escuchando..." + "\r\n";
+                Socket Escuchar = miPrimerSocket.Accept();
+                textBoxRespuesta.Text += "Conectado..." + "\r\n";
+                
+                while (true)
+                {
+                    ByRec = new byte[255];
+                    int a = Escuchar.Receive(ByRec, 0, ByRec.Length, 0);
+                    Array.Resize(ref ByRec, a);
+                    textBoxRespuesta.Text += "Cliente: " + Encoding.Default.GetString(ByRec)  + "\r\n"; //mostramos lo recibido
+                }
+
+            }
+            catch (Exception error)
+            {
+                textBoxRespuesta.Text +="Error: "+ error.ToString();
+            }finally{
+                miPrimerSocket.Close();
+            }           
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            miPrimerSocket.Close();
+        }
+    }
+}
